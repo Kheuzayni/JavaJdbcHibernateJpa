cf34e2211092029f9e313cdd83deb4ed5df2898e
unknown <bayecheikhfall249@gmail.com>
2022-11-22 11:42:07 +0000

    Tournoi Dto

--- src/main/java/com/mycompany/tennis/Dto/EpreuveDto.java
+++ src/main/java/com/mycompany/tennis/Dto/EpreuveDto.java
@@ -1,7 +1,51 @@
 package com.mycompany.tennis.Dto;
 
 import com.mycompany.tennis.entity.Epreuve;
+import com.mycompany.tennis.entity.Tournoi;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 public class EpreuveDto extends Epreuve {
+    private Long id;
+    private Short annee;
+    @Column(name = "TYPE_EPREUVE")
+    private Character TypeEpreuve;
+    //    @Transient
+    @ManyToOne
+    @JoinColumn(name = "ID_TOURNOI")
+    private Tournoi tournoi;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Short getAnnee() {
+        return annee;
+    }
+
+    public void setAnnee(Short annee) {
+        this.annee = annee;
+    }
+
+    public Character getTypeEpreuve() {
+        return TypeEpreuve;
+    }
+
+    public void setTypeEpreuve(Character typeEpreuve) {
+        TypeEpreuve = typeEpreuve;
+    }
+
+    public Tournoi getTournoi() {
+        return tournoi;
+    }
+
+    public void setTournoi(Tournoi tournoi) {
+        this.tournoi = tournoi;
+    }
 }
